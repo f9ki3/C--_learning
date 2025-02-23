b4d2ffa2180c4c2522712c0efd248a6272aa6da3
Fyke Lleva <110708999+f9ki3@users.noreply.github.com>
2025-02-24 01:47:56 +0800

    c++

---
+++ 1_input_output.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string greeting;
+    string name = "Fykie";
+    cout << "Enter a greetings: ";
+    cin >> greeting;
+    cout << greeting << " " << name << endl;
+
+}
---
+++ 2_variabels.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float tax = 12.00;
+    int age = 21;
+    string name = "Fykie";
+    char section = 'A';
+    bool isAlive = true;
+    double pi = 3.14159;
+
+    cout << "Hi" << " "<< name << "." << "Your turning " << age << ". " << "Your tax is " << tax << " %"<< "for your section " << section << ". Are you alive " << isAlive << ". Then your pi is " << pi << endl;
+
+}
