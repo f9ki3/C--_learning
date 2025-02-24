0c24818808bdbc3be0b8310783bfdd64c48da8e1
Fyke Lleva <110708999+f9ki3@users.noreply.github.com>
2025-02-24 09:46:26 +0800

    operators

---
+++ 3_operators.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a = 10, b=3;
+
+    //Arithmetic
+    cout << a + b << endl;
+    cout << a * b << endl;
+    cout << a / b << endl;
+    cout << a % b << endl;
+
+    //Comparison
+    cout << (a != b) << endl;
+    cout << (a == b) << endl;
+    cout << (a > b) << endl;
+    cout << (a < b) << endl;
+
+    //Logical
+    cout << (a != b) << endl;
+    cout << (a == b) << endl;
+    cout << (a > b) << endl;
+    cout << (a < b) << endl;
+
+    return 0;
+}
